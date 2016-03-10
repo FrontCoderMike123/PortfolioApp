79afdff1f13c2f1c8922c69d14ff4b097c9bf7ce
FrontCoderMike123 <michelBeaubien@hotmail.com>
2016-03-10 01:34:14 -0800

    Home page

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic'])
+var portfolio = angular.module('portfolio', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -13,3 +13,80 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+portfolio.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider){
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider.state('home',{
+		url: '/',
+		views: {
+			home: {
+				templateUrl: 'views/home.html',
+				controller: 'homeCTRL'
+			}
+		}
+	});
+
+	$stateProvider.state('websites',{
+		url: '/websites',
+		views: {
+			websites: {
+				templateUrl: 'views/websites.html',
+				controller: 'websitesCTRL'
+			}
+		}
+	});
+
+
+	$stateProvider.state('designs',{
+		url: '/designs',
+		views: {
+			designs: {
+				templateUrl: 'views/designs.html',
+				controller: 'designsCTRL'
+			}
+		}
+	});
+
+	$stateProvider.state('biography',{
+		url: '/biography',
+		views: {
+			biography: {
+				templateUrl: 'views/biography.html',
+				controller: 'biographyCTRL'
+			}
+		}
+	});
+
+	$stateProvider.state('contact',{
+		url: '/contact',
+		views: {
+			contact: {
+				templateUrl: 'views/contact.html',
+				controller: 'contactCTRL'
+			}
+		}
+	});
+
+}]);
+
+portfolio.controller('homeCTRL',['$scope','$http',function($scope,$http){
+
+}]);
+
+portfolio.controller('websitesCTRL',['$scope','$http',function($scope,$http){
+	
+}]);
+
+portfolio.controller('designsCTRL',['$scope','$http',function($scope,$http){
+	
+}]);
+
+portfolio.controller('biographyCTRL',['$scope','$http',function($scope,$http){
+	
+}]);
+
+portfolio.controller('contactCTRL',['$scope','$http',function($scope,$http){
+	
+}]);
