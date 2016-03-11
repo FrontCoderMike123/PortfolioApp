b38fb53cc4e81931c079acd0d93faccf7931929f
FrontCoderMike123 <michelBeaubien@hotmail.com>
2016-03-11 03:58:05 -0800

    Biography page and some Contact

--- www/js/app.js
+++ www/js/app.js
@@ -92,7 +92,7 @@ portfolio.config(['$stateProvider','$urlRouterProvider', function($stateProvider
 }]);
 
 portfolio.controller('homeCTRL',['$scope','$http',function($scope,$http){
-
+	$scope.PNG = "img/thumb.jpg";
 }]);
 
 portfolio.controller('websitesCTRL',['$scope','$http',function($scope,$http){
@@ -136,9 +136,30 @@ portfolio.controller('designDetailsCTRL',['$scope','$http','$stateParams',functi
 }]);
 
 portfolio.controller('biographyCTRL',['$scope','$http',function($scope,$http){
-	
+	$scope.ME = "img/me.jpg";
+	$scope.Born = "April 4th, 1990";
+	$scope.City = "London, Ontario";
+	$scope.Job = "Front-End Developer / Website Designer";
+
+	$scope.tools = [
+		{ "SVG":"img/tools/html5.svg","PNG":"img/tools/html5.png","Title":"HTML5" },
+		{ "SVG":"img/tools/css.svg","PNG":"img/tools/css.png","Title":"CSS3" },
+		{ "SVG":"img/tools/javascript.svg","PNG":"img/tools/javascript.png","Title":"Javascript" },
+		{ "SVG":"img/tools/mongo.svg","PNG":"img/tools/mongo.png","Title":"MongoDB" },
+		{ "SVG":"img/tools/express.svg","PNG":"img/tools/express.png","Title":"ExpressJS" },
+		{ "SVG":"img/tools/angular.svg","PNG":"img/tools/angular.png","Title":"AngularJS" },
+		{ "SVG":"img/tools/node.svg","PNG":"img/tools/node.png","Title":"NodeJS" }
+	];
+
+	$scope.trades = [
+		{ "SVG":"img/tools/sublime.svg","PNG":"img/tools/sublime.png","Title":"Sublime Text" },
+		{ "SVG":"img/tools/AI.svg","PNG":"img/tools/AI.png","Title":"Illustrator" },
+		{ "SVG":"img/tools/PS.svg","PNG":"img/tools/PS.png","Title":"Photoshop" },
+		{ "SVG":"img/tools/inDesign.svg","PNG":"img/tools/inDesign.png","Title":"InDesign" },
+		{ "SVG":"img/tools/AE.svg","PNG":"img/tools/AE.png","Title":"After Effects" }
+	];
 }]);
 
 portfolio.controller('contactCTRL',['$scope','$http',function($scope,$http){
-	
+	$scope.ME = "img/me.jpg";
 }]);
